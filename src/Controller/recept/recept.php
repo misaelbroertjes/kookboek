bc8a7f33241eebc3635404f94e341d48e904e5e9
misaelbroertjes <misaelbroertjes@hotmail.com>
2017-12-15 21:54:48 +0100

    stapjes

---
+++ src/Controller/recept/recept.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\recept;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class recept extends Controller{
+	/**
+	 * Matches /lucky exactly
+	 * @Route("/recept")
+	 */
+	public function showPage(){
+		return new Response('I am so lucky!');
+		//return $this->redirect('http://www.google.nl');
+		//return $this->redirectToRoute('unlucky');
+	}
+}
