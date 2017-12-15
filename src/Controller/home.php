dcccb0c4c45fd0a992f190052521f747b131c7c4
misaelbroertjes <misaelbroertjes@hotmail.com>
2017-12-15 17:12:51 +0100

    homepage werkt

--- src/Controller/home.php
+++ src/Controller/home.php
@@ -1,13 +1,15 @@
 <?php
 
+
 namespace App\Controller;
+
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class lucky {
+class home{
 	/**
-	 * @Route("/lucky")
+	 * @Route("/")
 	 */
 	public function showHome(){
 		return new Response('Under the sea!');
